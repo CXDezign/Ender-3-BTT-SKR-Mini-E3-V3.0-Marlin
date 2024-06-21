e90755a0e03c281286e78d32a93c52ea58aebf9b
CXDezign <kciepluch1998@gmail.com>
2024-06-22 01:57:45 +0200

    Update CR Touch Probe Offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1513,7 +1513,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, -1.85 }
+#define NOZZLE_TO_PROBE_OFFSET { -47, -4.2, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
