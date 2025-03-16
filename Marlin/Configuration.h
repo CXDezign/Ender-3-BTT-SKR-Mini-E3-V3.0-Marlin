bab5c8cfae42584e1fc280f497ee03a52588784c
CXDezign <account@cxdezign.dev>
2025-03-16 19:30:32 +0100

    Max Feedrate Limits

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1205,7 +1205,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 40, 25 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
